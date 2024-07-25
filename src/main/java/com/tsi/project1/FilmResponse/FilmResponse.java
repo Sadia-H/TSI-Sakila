7dfa48f9035bb05f7ec917094b6d5aa632879bdf
Sadia-H <sadiahussain118@gmail.com>
2024-07-25 22:56:24 +0100

    Added more film indetails to FilmResponse

--- src/main/java/com/tsi/project1/FilmResponse/FilmResponse.java
+++ src/main/java/com/tsi/project1/FilmResponse/FilmResponse.java
@@ -2,6 +2,7 @@ package com.tsi.project1.FilmResponse;
 
 import com.tsi.project1.ActorResponse.PartialActorResponse;
 import com.tsi.project1.Film.Film;
+import com.tsi.project1.Language.Language;
 import lombok.Getter;
 
 import java.time.Year;
@@ -13,6 +14,16 @@ public class FilmResponse {
     private final String title;
     private final Year releaseYear;
     private final List<PartialActorResponse> cast;
+    private final String description;
+    private final double rentalDuration;
+    private final double rentalRate;
+    private final int length;
+    private final double replacementCost;
+    private final String rating;
+    private final String specialFeatures;
+    private final Language language;
+
+
 
     public FilmResponse (Film film) {
         this.filmId = film.getFilmId();
@@ -22,6 +33,14 @@ public class FilmResponse {
                 .stream()
                 .map(PartialActorResponse::new)
                 .toList();
+        this.description = film.getDescription();
+        this.rentalDuration = film.getRentalDuration();
+        this.rentalRate = film.getRentalRate();
+        this.length = film.getLength();
+        this.replacementCost = film.getReplacementCost();
+        this.rating = film.getRating();
+        this.specialFeatures = film.getSpecialFeatures();
+        this.language = film.getLanguage();
 
     }
 }
